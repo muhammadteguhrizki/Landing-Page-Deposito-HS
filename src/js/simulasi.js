0f90cd3a6412dba06873f5c27249daedaf11c384
unknown <muhammadteguhrizki>
2024-03-28 14:46:36 +0700

    update simulasi

--- src/js/simulasi.js
+++ src/js/simulasi.js
@@ -10,13 +10,31 @@ function hitung() {
     bunga = 0.0675; // 6,75%
   }
 
-  // rumus deposito
-  var totalBunga = nominal * bunga * (tenor / 12);
-  var pajak = totalBunga * 0.2; // pajak  20%
+  // rumus deposito hariarta sedana
+  // var totalBunga = ((nominal * bunga * 30) / 366) * tenor;
+  var totalBunga = Math.round((nominal * bunga * 30) / 366) * tenor;
+  // var pajak = Math.round(totalBunga * 0.2); // pajak  20%
+
+  var pajak = 0;
+  if (totalBunga >= 7500000) {
+    // Jika totalBunga lebih besar dari atau sama dengan 7.5 juta
+    pajak = Math.round(totalBunga * 0.2); // pajak 20%
+  }
+
   var nilaiPajakBunga = totalBunga - pajak;
-  var totalAkhir = nominal + nilaiPajakBunga - pajak;
+  var totalAkhir = nominal + nilaiPajakBunga;
 
   var hasil = `
+    <div class="form-group mb-3">
+      <span>Nominal</span>
+      <h3>Rp ${nominal.toLocaleString()}</h3>
+    </div>
+    <hr>
+    <div class="form-group mb-3">
+        <span>Bunga</span>
+        <h3>${(bunga * 100).toFixed(2)}%</h3>
+    </div>
+    <hr>
     <div class="form-group mb-3">
       <span>Nilai bunga</span>
       <h3>Rp ${totalBunga.toLocaleString()}</h3>
@@ -67,10 +85,11 @@ document.getElementById("tenor").addEventListener("input", function () {
 
   var bunga;
   if (tenor <= 1) {
-    bunga = 6.0;
+    bunga = 0.06;
   } else {
-    bunga = 6.75;
+    bunga = 0.0675;
   }
 
-  document.getElementById("sukuBunga").textContent = bunga + " %";
+  document.getElementById("sukuBunga").textContent =
+    (bunga * 100).toFixed(2) + "%";
 });
