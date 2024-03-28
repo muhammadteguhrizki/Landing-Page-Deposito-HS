96c61dfdcfac9eec088588ad7251b376782c5f7d
unknown <muhammadteguhrizki>
2024-03-28 11:09:10 +0700

    update

--- src/js/simulasi.js
+++ src/js/simulasi.js
@@ -2,15 +2,13 @@ function hitung() {
   var nominal = document.getElementById("nominal").value * 10000000; // konversi ke nominal dalam rupiah
   var tenor = parseInt(document.getElementById("tenor").value);
   var bunga;
-  bunga = 0.0675;
 
-  // if (nominal <= 50000000) {
-  //   bunga = 0.05;
-  // } else if (nominal <= 500000000) {
-  //   bunga = 0.06;
-  // } else {
-  //   bunga = 0.07;
-  // }
+  // bunga = 0.0675;
+  if (tenor <= 1) {
+    bunga = 0.06; // 6%
+  } else {
+    bunga = 0.0675; // 6,75%
+  }
 
   // rumus deposito
   var totalBunga = nominal * bunga * (tenor / 12);
@@ -25,7 +23,7 @@ function hitung() {
     </div>
     <hr>
     <div class="form-group mb-3">
-      <span>Pajak bunga 20%</span>
+      <span>Bunga net</span>
       <h3>Rp ${nilaiPajakBunga.toLocaleString()}</h3>
     </div>
     <hr>
@@ -59,6 +57,20 @@ document.getElementById("nominalValue").addEventListener("input", function () {
   }
 });
 
+// document.getElementById("tenor").addEventListener("input", function () {
+//   document.getElementById("tenorValue").textContent = this.value + " bulan";
+// });
+
 document.getElementById("tenor").addEventListener("input", function () {
-  document.getElementById("tenorValue").textContent = this.value + " bulan";
+  var tenor = parseInt(this.value);
+  document.getElementById("tenorValue").textContent = tenor + " bulan";
+
+  var bunga;
+  if (tenor <= 1) {
+    bunga = 6.0;
+  } else {
+    bunga = 6.75;
+  }
+
+  document.getElementById("sukuBunga").textContent = bunga + " %";
 });
